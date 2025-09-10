01daaafc50ee584690c550e4f16af62f08bff029
Muhammed Bozan <bozan.mcan@gmail.com>
2025-09-10 15:04:47 -0400

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("First git java");
+        System.out.println("Hello Cydeo");
     }
 }
